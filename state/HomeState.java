9b0a3304ca9a2fd290aa34dea32557963e7eb37c
Kayly Tran <ktran7895@gmail.com>
2022-11-14 18:02:51 -0500

    HomeState methods

--- state/HomeState.java
+++ state/HomeState.java
@@ -3,23 +3,23 @@ public class HomeState implements State{
     private TV tv; 
 
     public String pressHomeButton() {
-
+        return "TV is already on the home screen";
     }
 
     public String pressNetflixButton() {
-
+        return super.pressNetflixButton();
     }
 
     public String pressHuluButton() {
-        
+        return super.pressHuluButton(); 
     }
 
     public String pressMovieButton() {
-
+        return super.pressMovieButton(); 
     }
 
     public String pressTVButton() {
-
+        return super.pressTVButton(); 
     }
 
 
