125b2448f9b57274ba26c039d023a81efed1404f
Kayly Tran <ktran7895@gmail.com>
2022-11-15 13:35:47 -0500

    TV.java methods

---
+++ state/TV.java
@@ -0,0 +1,52 @@
+public class TV {
+    
+    private State HomeState; 
+    private State NetflixState; 
+    private State HuluState; 
+    private State state; 
+
+    public TV() {
+        HomeState = new HomeState(this); 
+        NetflixState = new NetflixState(this); 
+        HuluState = new HuluState(this); 
+
+        state = HomeState; 
+    }
+
+    public String pressHomeButton() {
+        return state.pressHomeButton(); 
+    }
+
+    public String pressNetflixButton() {
+        return state.pressNetflixButton(); 
+    }
+
+    public String pressHuluButton() {
+        return state.pressHuluButton();
+    }
+
+    public String pressMovieButton() {
+        return pressMovieButton(); 
+    }
+
+    public String pressTVButton() {
+        return pressTVButton(); 
+    }
+
+    public void setState(State state) {
+        this.state = state; 
+    }
+
+    public State getHomeState() {
+        return HomeState; 
+    }
+
+    public State getNetflixState() {
+        return NetflixState; 
+    }
+
+    public State getHuluState() {
+        return HuluState; 
+    }
+    
+}
