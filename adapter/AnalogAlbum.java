4d55ecd9f8cd336fe08a83d81d9f27925cef3a53
Kayly Tran <ktran7895@gmail.com>
2022-11-21 15:53:20 -0500

    inital files

---
+++ adapter/AnalogAlbum.java
@@ -0,0 +1,8 @@
+public interface AnalogAlbum {
+    public String play(); 
+    public String rewind();
+    public Sting ffwd();
+    public String pause(); 
+    public String stopEject(); 
+    
+}
